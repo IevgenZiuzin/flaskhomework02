38f804971d14c2e209aef178a1402d165b24e85c
ievhen <zeugenz@gmail.com>
2023-03-06 19:18:56 +0200

    tasks 1-4

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ from flask import Flask, render_template, url_for
 from song.song import song
 from cars.cars import cars
 from weekday.weekday import weekday
-from books.books import books
-from headphones.headphones import headphones
 
 
 app = Flask(__name__)
 app.register_blueprint(song, url_prefix='/song')
 app.register_blueprint(cars, url_prefix='/cars')
 app.register_blueprint(weekday, url_prefix='/weekday')
-app.register_blueprint(books, url_prefix='/books')
-app.register_blueprint(headphones, url_prefix='/headphones')
 
 
 @app.route('/')
@@ -22,6 +18,7 @@ def index():
         'page_title': 'Home'
     }
     return render_template('index.html', context=context)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
